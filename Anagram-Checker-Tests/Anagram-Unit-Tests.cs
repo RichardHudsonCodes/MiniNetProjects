2cf6cd468ce958ba1045cd60080b59ad02cb9a22
Richard Hudson <richardhudson@Richards-MBP.cust.communityfibre.co.uk>
2022-05-04 16:35:50 +0100

    adding files

---
+++ Anagram-Checker-Tests/Anagram-Unit-Tests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using Anagram_Checker;
+
+namespace Anagram_Checker_Tests
+{
+    public class AnagramCheckerUnitTests
+    {
+        public AnagramChecker _anagramChecker = new AnagramChecker();
+
+        [Fact]
+        public void CheckAnagramsAreValid_ReturnsTrue_WhenValidAnagramsAreInputted()
+        {
+            //arrange
+            var anagram = "visualstudio" ;
+            var secondValidAnagram = "tdiisuluovsa";
+
+            //action
+            var anagramIsValid = _anagramChecker.CheckAnagramsAreValid(anagram, secondValidAnagram);
+
+            //assert
+            Assert.True(anagramIsValid);
+        }
+
+        [Fact]
+        public void CheckAnagramsAreValid_ReturnFalse_WhenInValidAnagramsAreInputted()
+        {
+            //arrange
+            var anagram = "visualstudio";
+            var secondInvalidAnagram = "tdiisuluovsq";
+
+            //action
+            var anagramIsValid = _anagramChecker.CheckAnagramsAreValid(anagram, secondInvalidAnagram);
+
+            //assert
+            Assert.False(anagramIsValid);
+        }
+
+        [Fact]
+        public void CheckAnagramsAreValid_ReturnFalse_WhenStringsWithDifferentLengthsAreTested()
+        {
+            //arrange
+            var anagram = "visualstudio";
+            var secondInvalidAnagram = "tdiisuluovsaas";
+
+            //action
+            var isValid = _anagramChecker.CheckAnagramsAreValid(anagram, secondInvalidAnagram);
+            //assert
+            Assert.False(isValid);
+        }
+    }
+    
+}
---
+++ Anagram-Checker/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anagram_Checker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class AnagramChecker
+    {
+        public AnagramChecker() { }
+
+        public bool CheckAnagramsAreValid(string anagram, string secondAnagram)
+        {
+            var anagramDic = CreateDictionary(anagram);
+            var secondAnagramDic = CreateDictionary(secondAnagram);
+
+            return AreDictionariesEqual(anagramDic, secondAnagramDic); 
+        }
+
+        private Dictionary<char, int> CreateDictionary(string anagram)
+        {
+            var anagramDic = new Dictionary<char, int>();
+
+            foreach (char character in anagram)
+            {
+                if (anagramDic.ContainsKey(character))
+                {
+                    anagramDic[character] += 1;
+                }
+                else
+                {
+                    anagramDic.Add(character, 1);
+                }
+            }
+
+            return anagramDic;
+        }
+
+        private bool AreDictionariesEqual(Dictionary<char, int> firstDictionary, Dictionary<char, int> secondDictionary)
+        {
+            if (firstDictionary == null || secondDictionary == null) return false;
+            if (firstDictionary.Count != secondDictionary.Count) return false;
+
+            foreach (char character in firstDictionary.Keys)
+            {
+                var correspondingKey = secondDictionary.GetValueOrDefault(character);
+                if (firstDictionary[character] != correspondingKey)
+                {
+                    return false;
+                }
+            }
+
+            return true; 
+        }
+    }
+}
